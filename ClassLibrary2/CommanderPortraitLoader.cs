a569aaa0ff166768173db86839a5f5ea6c0fadc1
Morphyum <zion@c-schwabe.de>
2018-05-06 13:46:15 +0200

    *added own manifest and moved all needed files to mod folder

--- ClassLibrary2/CommanderPortraitLoader.cs
+++ ClassLibrary2/CommanderPortraitLoader.cs
@@ -12,27 +12,22 @@ using UnityEngine.Rendering;
 using BattleTech.UI;
 using HBS;
 using HBS.Collections;
+using BattleTech.Data;
 
-namespace CommanderPortraitLoader
-{
+namespace CommanderPortraitLoader {
     [HarmonyPatch(typeof(RenderedPortraitResult), "get_Item")]
-    public static class RenderedPortraitResult_get_Item_Patch
-    {
-        static void Postfix(RenderedPortraitResult __instance, ref Texture2D __result)
-        {
+    public static class RenderedPortraitResult_get_Item_Patch {
+        static void Postfix(RenderedPortraitResult __instance, ref Texture2D __result) {
             Logger.LogLine("RenderedPortraitResult Postfix start");
-            if (!string.IsNullOrEmpty(__instance.settings.Description.Icon))
-            {
+            if (!string.IsNullOrEmpty(__instance.settings.Description.Icon)) {
                 Logger.LogLine("Icon Found");
-                try
-                {
+                try {
                     Texture2D texture2D = new Texture2D(2, 2);
                     byte[] array = File.ReadAllBytes("mods/CommanderPortraitLoader/Portraits/" + __instance.settings.Description.Icon + ".png");
                     texture2D.LoadImage(array);
                     __result = texture2D;
                 }
-                catch (Exception e)
-                {
+                catch (Exception e) {
                     Logger.LogError(e);
                 }
             }
@@ -40,15 +35,11 @@ namespace CommanderPortraitLoader
     }
 
     [HarmonyPatch(typeof(SGCharacterCreationWidget), "CreatePilot")]
-    public static class SGCharacterCreationWidget_CreatePilot_Patch
-    {
-        static void Postfix(ref SGCharacterCreationWidget __instance, ref Pilot __result)
-        {
+    public static class SGCharacterCreationWidget_CreatePilot_Patch {
+        static void Postfix(ref SGCharacterCreationWidget __instance, ref Pilot __result) {
             Logger.LogLine("CreatePilot Postfix start");
-            try
-            {
-                if (!string.IsNullOrEmpty(__result.pilotDef.Description.Icon))
-                {
+            try {
+                if (!string.IsNullOrEmpty(__result.pilotDef.Description.Icon)) {
                     PilotDef pilotDef = new PilotDef(new HumanDescriptionDef(__result.Description.Id, __result.Description.Callsign, __result.Description.FirstName, __result.Description.LastName,
                         __result.Description.Callsign, __result.Description.Gender, Faction.NoFaction, __result.Description.Age, __result.Description.Details, __result.pilotDef.Description.Icon),
                         __result.Gunnery, __result.Piloting, __result.Guts, __result.Guts, 0, 3, false, 0, string.Empty, CommanderPortraitLoader.GetAbilities(__result.Gunnery, __result.Piloting, __result.Guts, __result.Guts), AIPersonality.Undefined, 0, __result.pilotDef.PilotTags, 0, 0);
@@ -58,23 +49,18 @@ namespace CommanderPortraitLoader
                     Logger.LogLine("Pilot modified");
                 }
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Logger.LogError(e);
             }
         }
     }
 
     [HarmonyPatch(typeof(PilotDef), "GetPortraitSprite")]
-    public static class PilotDef_GetPortraitSprite_Patch
-    {
-        static void Postfix(ref PilotDef __instance, ref Sprite __result)
-        {
+    public static class PilotDef_GetPortraitSprite_Patch {
+        static void Postfix(ref PilotDef __instance, ref Sprite __result) {
             Logger.LogLine("GetPortraitSprite Postfix start");
-            try
-            {
-                if (__result == null)
-                {
+            try {
+                if (__result == null) {
                     Texture2D texture2D2 = new Texture2D(2, 2);
                     byte[] data = File.ReadAllBytes("mods/CommanderPortraitLoader/Portraits/" + __instance.Description.Icon + ".png");
                     texture2D2.LoadImage(data);
@@ -83,23 +69,18 @@ namespace CommanderPortraitLoader
                     __result = sprite;
                 }
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Logger.LogError(e);
             }
         }
     }
 
     [HarmonyPatch(typeof(PilotDef), "GetPortraitSpriteThumb")]
-    public static class PilotDef_GetPortraitSpriteThumb_Patch
-    {
-        static void Postfix(ref PilotDef __instance, ref Sprite __result)
-        {
+    public static class PilotDef_GetPortraitSpriteThumb_Patch {
+        static void Postfix(ref PilotDef __instance, ref Sprite __result) {
             Logger.LogLine("GetPortraitSpriteThumb Postfix start");
-            try
-            {
-                if (__result == null)
-                {
+            try {
+                if (__result == null) {
                     Texture2D texture2D2 = new Texture2D(2, 2);
                     byte[] data = File.ReadAllBytes("mods/CommanderPortraitLoader/Portraits/" + __instance.Description.Icon + ".png");
                     texture2D2.LoadImage(data);
@@ -108,26 +89,64 @@ namespace CommanderPortraitLoader
                     __result = CommanderPortraitLoader.DownsampleSprite(sprite);
                 }
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Logger.LogError(e);
             }
         }
     }
 
-    public static class CommanderPortraitLoader
-    {
+    /*[HarmonyPatch(typeof(SGCharacterCreationPortraitSelectionPanel), "PopulateList")]
+    public static class SGCharacterCreationPortraitSelectionPanel_PopulateList_Patch {
+        static void Postfix(SGCharacterCreationPortraitSelectionPanel __instance) {
+            Logger.LogLine("PopulateList Prefix start");
 
-        public static void Init()
-        {
+            try {
+                PortraitSettings settings = new PortraitSettings();
+                string json;
+                using (StreamReader r = new StreamReader("mods/CommanderPortraitLoader/Portraits/PortraitPreset_Kara.json")) {
+                    json = r.ReadToEnd();
+                }
+                settings.FromJSON(json);
+                DataManager data = LazySingletonBehavior<UnityGameInstance>.Instance.Game.DataManager;
+                KeyValuePair<string, PortraitSettings> pair = new KeyValuePair<string, PortraitSettings>(settings.Description.Id, settings);
+                data.PortraitSettings.Add(pair);
+            }
+            catch (Exception e) {
+
+                Logger.LogError(e);
+            }
+        }
+    }*/
+
+    [HarmonyPatch(typeof(VersionManifestUtilities), "LoadDefaultManifest")]
+    public static class VersionManifestUtilitiesPatch {
+        // ReSharper disable once RedundantAssignment
+        public static void Postfix(ref VersionManifest __result) {
+            try {
+                var addendum = VersionManifestUtilities.ManifestFromCSV("mods/CommanderPortraitLoader/VersionManifest.csv");
+                foreach (var entry in addendum.Entries) {
+                    __result.AddOrUpdate(entry.Id, entry.FilePath, entry.Type, entry.AddedOn, entry.AssetBundleName, entry.IsAssetBundlePersistent);
+                }
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+            }
+
+        }
+    }
+
+    public static class CommanderPortraitLoader {
+
+        public static void Init() {
             var harmony = HarmonyInstance.Create("de.morphyum.CommanderPortraitLoader");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
 
         }
 
+        
+
         // Token: 0x0600564D RID: 22093 RVA: 0x0023F158 File Offset: 0x0023D358
-        public static Sprite DownsampleSprite(Sprite oldSprite)
-        {
+        public static Sprite DownsampleSprite(Sprite oldSprite) {
             Texture2D texture = oldSprite.texture;
             RenderTexture temporary = RenderTexture.GetTemporary(texture.width / 2, texture.height / 2, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB);
             RenderTexture temporary2 = RenderTexture.GetTemporary(texture.width / 4, texture.height / 4, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB);
@@ -144,8 +163,7 @@ namespace CommanderPortraitLoader
             return Sprite.Create(texture2D, new Rect(0f, 0f, (float)texture2D.width, (float)texture2D.height), new Vector2(0.5f, 0.5f), 100f, 0u, SpriteMeshType.FullRect, Vector4.zero);
         }
 
-        public static List<string> GetAbilities(int gunnery, int piloting, int guts, int tactics)
-        {
+        public static List<string> GetAbilities(int gunnery, int piloting, int guts, int tactics) {
             List<string> list = new List<string>();
             SimPilotProgressionConstantsDef progression = LazySingletonBehavior<UnityGameInstance>.Instance.Game.Simulation.Constants.Progression;
             int abilityLevel = gunnery;
@@ -159,11 +177,9 @@ namespace CommanderPortraitLoader
             return list;
         }
 
-        public static List<string> GetAbilityDefsForSkill(string[][] abilityDefConsts, int abilityLevel)
-        {
+        public static List<string> GetAbilityDefsForSkill(string[][] abilityDefConsts, int abilityLevel) {
             List<string> list = new List<string>();
-            for (int i = 0; i < abilityLevel; i++)
-            {
+            for (int i = 0; i < abilityLevel; i++) {
                 list.AddRange(abilityDefConsts[i]);
             }
             return list;
@@ -191,24 +207,19 @@ namespace CommanderPortraitLoader
          public float RecoveryChance;
      }*/
 
-    public class Logger
-    {
-        public static void LogError(Exception ex)
-        {
+    public class Logger {
+        public static void LogError(Exception ex) {
             string filePath = "mods/CommanderPortraitLoader/Log.txt";
-            using (StreamWriter writer = new StreamWriter(filePath, true))
-            {
+            using (StreamWriter writer = new StreamWriter(filePath, true)) {
                 writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
                    "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
                 writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
             }
         }
 
-        public static void LogLine(String line)
-        {
+        public static void LogLine(String line) {
             string filePath = "mods/CommanderPortraitLoader/Log.txt";
-            using (StreamWriter writer = new StreamWriter(filePath, true))
-            {
+            using (StreamWriter writer = new StreamWriter(filePath, true)) {
                 writer.WriteLine(line + Environment.NewLine + "Date :" + DateTime.Now.ToString());
                 writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
             }
