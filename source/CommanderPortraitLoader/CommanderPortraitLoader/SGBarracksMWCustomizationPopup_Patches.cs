ba03b3583999a3340e00084246a3be6b30ceb45d
Bill Torode <wmtorode@gmail.com>
2019-06-20 01:32:46 -0400

    fixed up barracks customization

---
+++ source/CommanderPortraitLoader/CommanderPortraitLoader/SGBarracksMWCustomizationPopup_Patches.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Harmony;
+using BattleTech;
+using BattleTech.UI;
+using BattleTech.Portraits;
+
+namespace CommanderPortraitLoader
+{
+    [HarmonyPatch(typeof(SGBarracksMWCustomizationPopup), "Save")]
+    public static class SGBarracksMWCustomizationPopup_SavePatch
+    {
+        static void Postfix(ref SGBarracksMWCustomizationPopup __instance)
+        {
+            if (!string.IsNullOrEmpty(NewVoice.newVoice))
+            {
+                __instance.pilot.pilotDef.SetVoice(NewVoice.newVoice);
+            }
+            if (!string.IsNullOrEmpty(__instance.pilot.pilotDef.Description.Icon))
+            {
+                __instance.pilot.pilotDef.PortraitSettings = null;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SGBarracksMWCustomizationPopup), "LoadPortraitSettings")]
+    public static class SGBarracksMWCustomizationPopup_LoadPortraitSettingsPatch
+    {
+        static void Prefix(ref SGBarracksMWCustomizationPopup __instance, ref PortraitSettings portraitSettingsData)
+        {
+            try
+            {
+                if (portraitSettingsData == null)
+                {
+                    if(!string.IsNullOrEmpty(__instance.pilot.pilotDef.Description.Icon))
+                    {
+                        string filePath = $"{ CommanderPortraitLoader.ModDirectory}/PortraitJsons/" + __instance.pilot.pilotDef.Description.Icon + ".json";
+                        if (File.Exists(filePath))
+                        {
+                            portraitSettingsData = new PortraitSettings();
+                            using (StreamReader r = new StreamReader(filePath))
+                            {
+                                string json = r.ReadToEnd();
+                                portraitSettingsData.FromJSON(json);
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+        }
+    }
+}
---
+++ source/CommanderPortraitLoader/CommanderPortraitLoader/SGBarracksMWDetailPanel_Patches.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using BattleTech;
+using BattleTech.UI;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CommanderPortraitLoader
+{
+
+
+    [HarmonyPatch(typeof(SGBarracksMWDetailPanel), "CustomizePilot")]
+    public static class SGBarracksMWDetailPanel_CustomizePilotPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            int startIndex = -1;
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldfld)
+                {
+                    startIndex = i;
+                    break;
+                }
+            }
+            if (startIndex > -1)
+            {
+                codes.RemoveRange(startIndex + 1, 8);
+                codes.Insert(startIndex + 1, new CodeInstruction(OpCodes.Ldc_I4_0));
+            }
+            return codes.AsEnumerable();
+        }
+    }
+}
